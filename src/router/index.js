9c3bb749d5217d2c57f8fd80e1171e78c1e865a4
estefanitapia <estefani.tapia.godoy@gmail.com>
2024-07-19 20:28:00 -0400

    agregando ultimos cambios

--- src/router/index.js
+++ src/router/index.js
@@ -5,26 +5,53 @@ import HomeView from '../views/HomeView.vue'
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: HomeView
   },
   {
-    path: "/productos",
-    name: "Productos",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/ProductosView.vue"),
+    path: '/contacto',
+    name: 'contacto',
+    component: () => import('../views/ContactoView.vue')
   },
   {
-    path: "/contacto",
-    name: "Contacto",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/ContactoView.vue"),
+    path: '/productos',
+    name: 'Productos',
+    component: () => import('../views/ProductosView.vue'),
+    props: (route) => {
+      return {
+        productos: [
+          {
+            id: 1,
+            imagen: require('../assets/img/desayunos.jpg'),
+            nombre: 'Desayuno',
+            descripcion: 'Contamos con desayunos nutritivos y deliciosos',
+            precio: 2000,
+            cantidad: 'Para dos personas'
+          },
+          {
+            id: 2,
+            imagen: require('../assets/img/bebidas.jpg'),
+            nombre: 'Bebidas',
+            descripcion: 'Contamos con ricas bebidas',
+            precio: 1000,
+            cantidad: 'Para una persona'
+          },
+          {
+            id: 3,
+            imagen: require('../assets/img/empanadas.jpg'),
+            nombre: 'Empanadas',
+            descripcion: 'Contamos con ricas empanadas',
+            precio: 1500,
+            cantidad: 'Para una persona'
+          }
+        ]
+      }
+    }
   },
-  
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
--- src/views/ProductosView.vue
+++ src/views/ProductosView.vue
@@ -1,75 +1,53 @@
 <template>
-    <section  class="container" id="productos">
-      <h1 class="text-center">Nuestros Productos</h1>
-      <div class="row">
-        <div class="col-sm-4 col-12 px-5 mb-4" v-for="producto in productos" :key="producto.id">
-          <div class="card">
-            <img :src="producto.imagen" :alt="producto.nombre">
-            <div class="card-body">
-              <h5 class="card-title fw-bold">{{ producto.nombre }}</h5>
-              <p class="card-text">{{ producto.descripcion }}</p>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">Precio: ${{ producto.precio }}</li>
-              <li class="list-group-item">Cantidad disponible: {{ producto.cantidad }}</li>
-            </ul>
-            
+  <section class="container" id="productos">
+    <h1 class="text-center">Nuestros Productos</h1>
+    <div class="row">
+      <div class="col-sm-4 col-12 px-5 mb-4" v-for="producto in productos" :key="producto.id">
+        <div class="card">
+          <img :src="producto.imagen" :alt="producto.nombre" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title fw-bold">{{ producto.nombre }}</h5>
+            <p class="card-text">{{ producto.descripcion }}</p>
           </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Precio: ${{ producto.precio }}</li>
+            <li class="list-group-item">Cantidad disponible: {{ producto.cantidad }}</li>
+          </ul>
         </div>
       </div>
-    </section>
-    <div class="card-body">
-                <button class="button-index" @click="volverInicio"> Volver al Inicio</button>
-            </div>
-  </template>
-  
-  <script>
-  export default {
-    methods: {
-        volverInicio() {
-            return this.$router.push('/');
-        },
-    },
-    data() {
-      return {
-        productos: [
-          {
-            id: 1,
-            imagen: require('../assets/img/desayunos.jpg'),
-            nombre: 'Desayuno',
-            descripcion: 'Contamos con desayunos nutritivos y deliciosos',
-            precio: 2000,
-            cantidad: 'Para dos personas'
-          },
-          {
-            id: 2,
-            imagen: require("../assets/img/bebidas.jpg"),
-            nombre: 'Bebidas',
-            descripcion: 'Contamos con ricas bebidas',
-            precio: 1000,
-            cantidad: 'Para una persona'
-          },
-          {
-            id: 3,
-            imagen: require("../assets/img/empanadas.jpg"),
-            nombre: 'Empanadas',
-            descripcion: 'Contamos con ricas empanadas',
-            precio: 1500,
-            cantidad: 'Para una persona'
-          },
+    </div>
+    <div class="card-body text-center">
+      <button class="button-index" @click="volverInicio">Volver al Inicio</button>
+    </div>
+  </section>
+</template>
 
-        ]
-      }
+<script>
+export default {
+  name: "ProductosView",
+  props: ["productos"],
+  methods: {
+    volverInicio() {
+      this.$router.push('/');
     }
+  },
+  created() {
+    console.log(this.productos);
   }
-  </script>
-  
-  <style>
-  .button-index {
-    margin-top: 10px;
-    margin-left: 5px;
-    padding: 5px;
-    color: white;
-    background-color: #e83e8c;
 }
-  </style>
+</script>
+
+<style>
+.button-index {
+  margin-top: 10px;
+  padding: 10px 20px;
+  color: white;
+  background-color: #e83e8c;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+.button-index:hover {
+  background-color: #d82a7a;
+}
+</style>
